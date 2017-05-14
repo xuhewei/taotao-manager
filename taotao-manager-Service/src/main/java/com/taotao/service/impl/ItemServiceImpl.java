09897c3a09807fded4de834ad2cac99bf8b278b7
xuhewei <1037901372@qq.com>
2017-05-14 16:21:14 +0800

    step1

---
+++ taotao-manager-Service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,44 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
+import com.taotao.service.ItemService;
+
+/**
+ * 商品查询Service
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.com</p> 
+ * @author	入云龙
+ * @date	2015年11月11日下午4:28:55
+ * @version 1.0
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	@Override
+	public TbItem getItemById(Long itemId) {
+		//TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		TbItemExample example = new TbItemExample();
+		//创建查询条件
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//判断list中是否为空
+		TbItem item = null;
+		if (list != null && list.size() > 0) {
+			item = list.get(0);
+		}
+		return item;
+	}
+
+}
