5237baddea1a9e99249c07a9e4aa180b9be96874
xhw1990514 <1037901372@q.com>
2017-06-20 17:51:33 +0800

    1

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/ContentCategoryController.java
@@ -0,0 +1,29 @@
+package com.taotao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.service.ContentCatgoryService;
+
+@Controller
+@RequestMapping("/content/category")
+public class ContentCategoryController {
+
+	@Autowired
+	private ContentCatgoryService contentCatgoryService;
+	
+	@RequestMapping("/list")
+	@ResponseBody
+	public List<EasyUITreeNode> getContentCatList(@RequestParam(value="id", defaultValue="0")Long parentId) {
+		List<EasyUITreeNode> list = contentCatgoryService.getContentCatList(parentId);
+		return list;
+		
+	}
+}
+
